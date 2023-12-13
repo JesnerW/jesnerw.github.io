0b1860647bbd707be8aeb5d72c7d0fbe508f84e9
Jesner <jesner631@gmail.com>
2023-12-12 21:50:54 -0500

    Updates

--- server/assets/entry-styles.248c2b5d.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './entry-styles-1.mjs-ca9ca359.js';
-export default [style_0]
--- server/styles.mjs
+++
@@ -1,9 +0,0 @@
-const interopDefault = r => r.default || r || []
-export default {
-  "node_modules/nuxt/dist/app/entry.js": () => import('./assets/entry-styles.248c2b5d.mjs').then(interopDefault),
-  "pages/index.vue": () => import('./assets/index-styles.838bf452.mjs').then(interopDefault),
-  "node_modules/nuxt-icon/dist/runtime/Icon.vue": () => import('./assets/Icon-styles.5a0fe09d.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./assets/error-404-styles.1e0dde27.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./assets/error-500-styles.cf4b3e80.mjs').then(interopDefault),
-  "node_modules/nuxt-icon/dist/runtime/IconCSS.vue": () => import('./assets/IconCSS-styles.6ad8301c.mjs').then(interopDefault)
-}
